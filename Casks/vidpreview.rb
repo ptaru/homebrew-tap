50ac259753a7ff7e6f27b06df3fb56eb78562a6a
Tarun <me@tarun.uk>
2026-02-10 23:04:01 +0000

    Update VidPreview to version 5.0, add smaller arm64-only version

--- Casks/vidpreview.rb
+++ Casks/vidpreview.rb
@@ -1,8 +1,15 @@
 cask "vidpreview" do
-  version "4.1"
-  sha256 "261ac4149da1762eaa9e98c2e193da4d3df69abef8ebcecb1ecf50032666166f"
+  version "5.0"
 
-  url "https://github.com/ptaru/VidPreview/releases/download/#{version}/VidPreview.zip"
+  on_arm do
+    sha256 "4e20908f96ced37ef8dc2144549bc8ef6dc46e8dc998a7e0e2f5d400ceb946c3"
+    url "https://github.com/ptaru/VidPreview/releases/download/#{version}/VidPreview_arm64.zip"
+  end
+  on_intel do
+    sha256 "3a67210f2882fd83dc3ee7e28bdd5b375ecdc8c324c777a92ee502e8c4970813"
+    url "https://github.com/ptaru/VidPreview/releases/download/#{version}/VidPreview_universal.zip"
+  end
+
   name "VidPreview"
   desc "QuickLook extension for native video preview"
   homepage "https://github.com/ptaru/VidPreview"
