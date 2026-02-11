89cf9e7dd721040c8a6753b8a096bc792441f739
Tarun <me@tarun.uk>
2026-02-11 18:39:35 +0000

    Update VidPreview to 5.1

--- Casks/vidpreview.rb
+++ Casks/vidpreview.rb
@@ -1,12 +1,14 @@
 cask "vidpreview" do
-  version "5.0"
+  version "5.1"
 
   on_arm do
-    sha256 "4e20908f96ced37ef8dc2144549bc8ef6dc46e8dc998a7e0e2f5d400ceb946c3"
+    sha256 "f429f63013dac645dc0777cfc76799a2c6f19366ea875a39afded0823c5c24a2"
+
     url "https://github.com/ptaru/VidPreview/releases/download/#{version}/VidPreview_arm64.zip"
   end
   on_intel do
-    sha256 "3a67210f2882fd83dc3ee7e28bdd5b375ecdc8c324c777a92ee502e8c4970813"
+    sha256 "6e2a007a8f85b3abd524553f5d05b06869588cb1a6e413bbe51b787f87dbf8be"
+
     url "https://github.com/ptaru/VidPreview/releases/download/#{version}/VidPreview_universal.zip"
   end
 
