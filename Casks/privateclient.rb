3b7cad308db7075cc771b0c16cbc1daa1c74ece8
Tarun <me@tarun.uk>
2026-04-22 19:10:38 +0100

    Update SHA256 checksum for privateclient cask

--- Casks/privateclient.rb
+++ Casks/privateclient.rb
@@ -1,6 +1,6 @@
 cask "privateclient" do
   version "1.0b4"
-  sha256 "864dbf65ea535f2ee3100964319b7fc8b864785ee07510984c1a67dc9a940015"
+  sha256 "c2e2507dffb77288895bd0f3a097bd3733cd387e9a9227d14e80e42fe72f7f32"
 
   url "https://github.com/ptaru/PrivateClient/releases/download/#{version}/PrivateClient-Release.zip"
   name "PrivateClient"
